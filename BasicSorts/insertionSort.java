570f82d21b7368d44b9c2436d0e892cba983b40e
arunmudhirajcoding <duruguarun@gmail.com>
2025-01-13 16:51:19 +0530

    added note insertion sort

--- BasicSorts/insertionSort.java
+++ BasicSorts/insertionSort.java
@@ -4,15 +4,21 @@ public class insertionSort {
     public static void main(String[] args) {
      int nums[] = {5, 4, 1, 6, 2};
         int n =nums.length;
-        // elements from 0th index to n-2th element
+        //looping from 1st index to last
         for (int i = 1; i < n; i++) {
-            //loop to find min element
+            //storing element
             int curr = nums[i];
             int prev = i-1;
+            //comparing previous elements with stored element
             while(prev >= 0&& nums[prev] > curr){
+                
+                //pushing previous element to front
                 nums[prev+1] = nums[prev];
+
+                // going to previous index 
                 prev --;
             }
+            // fill the space with stored element
             nums[prev+1] = curr;
         }
         for (int i = 0; i < n; i++) {
