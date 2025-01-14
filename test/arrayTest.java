0f620fdd8544fad1a075a81bf5e78dc226fce73d
arunmudhirajcoding <duruguarun@gmail.com>
2025-01-14 13:44:20 +0530

    #

---
+++ test/arrayTest.java
@@ -0,0 +1,9 @@
+
+public class arrayTest {
+    public static void main(String[] args) {
+        int nums[] = new int[7];
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
