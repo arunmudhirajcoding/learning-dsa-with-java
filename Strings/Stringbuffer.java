df4fd650e6e051f33c6ef2dc87efef0afc355900
arunmudhirajcoding <duruguarun@gmail.com>
2025-01-17 19:53:49 +0530

    understanding string buffer and builder

---
+++ Strings/Stringbuffer.java
@@ -0,0 +1,9 @@
+public class Stringbuffer {
+    public static void main(String[] args) {
+            //If thread safety is not a requirement, always prefer StringBuilder for better performance.
+            
+            // If the code needs to handle multiple threads modifying a single string, use StringBuffer to avoid concurrency issues.
+
+            //both are same use case are different
+    }
+}
