0b009e1e82d6e0ed6c8d4ae8f5fa5f610f6e4f28
arunmudhirajcoding <duruguarun@gmail.com>
2025-01-26 00:58:52 +0530

    mt code do it letter

---
+++ Recursion2/friendsPairing.java
@@ -0,0 +1,13 @@
+public class friendsPairing {
+//     Friends Pairing Problem
+// Given n friends, each one can remain single or can be paired up with some other
+// friend. Each friend can be paired only once. Find out the total number of ways in
+// which friends can remain single or can be paired up.
+
+//if 1 mem = ways 1 ie single
+// if 2 mem = 2 singles or 1 pair
+// if 3 mem = 1 single 2 pairs * 3, or 3 singles 
+    public static void main(String[] args) {
+        
+    }
+}
