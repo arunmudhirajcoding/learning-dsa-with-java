15292990e12cb112d6201e86e25d8d91bb1673f7
arunmudhirajcoding <duruguarun@gmail.com>
2025-02-07 18:11:21 +0530

    learnt quick sort

--- divide&conquer/QuickSort.java
+++ divide&conquer/QuickSort.java
@@ -4,31 +4,54 @@ public class QuickSort {
         if (si>=ei) {
             return;
         }
-        int pidx = partition(arr,si,ei);
+        int pidx = partition(arr,si,ei);// paritioning the arr
         quick(arr, si, pidx-1);
         quick(arr, pidx+1, ei);
 
     }
     static int partition(int[] arr, int si, int ei){
-        int pivot = arr[ei];
-        int i = si-1;
-        for (int j = si; j < ei; j++) {
-            if (arr[j] <= pivot) {
-                i++;
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
-            }
+        int pivot = arr[ei];// our pivot element is last element 
+        //method -2
+        int l = si,r =ei;
+        while (l<r) {
+            if (arr[l]> pivot) {
+                int temp = arr[l];
+                arr[l]=pivot;
+                arr[r]=temp;
+                r--;
+            } else if (arr[r]<pivot) {
+                int temp = arr[r];
+                arr[r]=pivot;
+                arr[l]=temp;
+                l++;
+            }else{
+                if (arr[l]<pivot) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }    
         }
-        i++;
-        int temp = pivot;
-            arr[ei] = arr[i];
-            arr[i] = temp;
+        
+        // Method-1 (efficient way do to less conditions)
+        // int i = si-1; // let the i index at outof the array
+        // for (int j = si; j < ei; j++) { //looping over the array
+        //     if (arr[j] <= pivot) {
+        //         i++; 
+        //         int temp = arr[j]; //swapping 2 adjucent elements
+        //         arr[j] = arr[i];
+        //         arr[i] = temp;
+        //     }
+        // }
+        // i++;
+        // int temp = pivot;
+        //     arr[ei] = arr[i];
+        //     arr[i] = temp;
 
-        return i;
+        return l;
     }
     public static void main(String[] args) {
-        int[] arr = {3,6,10,4,8,9};
+        int[] arr = {5, 4, 3, 2, 1};
         quick(arr, 0, arr.length-1);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
