4edbf084b395167c321142c962b5d26b32b36186
arunmudhirajcoding <duruguarun@gmail.com>
2025-05-20 11:52:50 +0530

    tested arrays initialize

--- test/arrayTest.java
+++ test/arrayTest.java
@@ -2,8 +2,15 @@
 public class arrayTest {
     public static void main(String[] args) {
         int nums[] = new int[7];
+        String str[] = new String[10];
+        double doub[] = new double[10];
+        boolean bool[] = new boolean[10];
+        
         for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i]+" ");
+            System.out.println(str[i]+" ");// null in all storage
+            System.out.println(doub[i]+" ");//0.0
+            System.out.println(bool[i]+" ");//false
+            System.out.print(nums[i]+" ");// without initiaizes array for int type storees zero's
         }
     }
 }
