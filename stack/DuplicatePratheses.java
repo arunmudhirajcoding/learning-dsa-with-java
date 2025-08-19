f2ef00ee0d0928f6155870585af4a98372163447
arunmudhirajcoding <duruguarun@gmail.com>
2025-08-19 20:04:21 +0530

    added notes

--- stack/DuplicatePratheses.java
+++ stack/DuplicatePratheses.java
@@ -5,21 +5,23 @@ public class DuplicatePratheses {
     //idea: b/w pais of paratheses something exist is vaild
     // (a+b) vaild something is a+b
     // ((b)) in valid (b) - ( ) after pair of b is elemenet outer is mt 
+    //couting inside elements. if count is not 0 means something is there then check for next iterative.
+    // if count is 0 then return true for it is duplicate 
     public static boolean isDuplicate(String str){
         Stack<Character> s = new Stack<>();
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {//iterative on string
             char ch = str.charAt(i);
-            if (ch == ')') {
+            if (ch == ')') {//if close then remove the elements and count
                 int count = 0;
                 while (s.pop()!='(') {
                     s.pop();
                     count++;
                 }
-                if (count<1) {
+                if (count<1) {//count is 0 then duplicate if not go next 
                     return true;
                 }
             }
-            else{
+            else{// if not close then push into stack
                 s.push(ch);
             }
         }
