845bc28ff6541a7ea90babb8103797654836c25d
arunmudhirajcoding <duruguarun@gmail.com>
2025-08-21 09:36:33 +0530

    greedy practiced

---
+++ Greedy/Intro.java
@@ -0,0 +1,19 @@
+
+public class Intro {
+    
+    //take a best choice (max or min )
+    //eg: company HR       | greedy | fair  
+    //Round 1: Online Test | (1000) | 1000
+    //Round 1: Interview   | (100 ) | 1000
+    //Round 1: Interview HR| (20 )  | 1000  
+    //it took greedy approach bcoz it take less time and efficent mem 
+    
+// where we use ? 1. optimization : min, max finding
+                //2. sorting approach
+                //3. No Fixed rule
+                //4.Not realizes that we used greedy in problem but practicing many problem can take greep on it
+
+    public static void main(String[] args) {
+        
+    }
+}
