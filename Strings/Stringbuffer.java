63dcd67d36f7b3ee9686a156d96259b3c93e5e4f
arunmudhirajcoding <duruguarun@gmail.com>
2026-05-25 10:05:47 +0530

    deleted and transfer files

--- Strings/Stringbuffer.java
+++
@@ -1,9 +0,0 @@
-public class Stringbuffer {
-    public static void main(String[] args) {
-            //If thread safety is not a requirement, always prefer StringBuilder for better performance.
-            
-            // If the code needs to handle multiple threads modifying a single string, use StringBuffer to avoid concurrency issues.
-
-            //both are same use case are different
-    }
-}
